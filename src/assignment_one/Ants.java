b634d146d2c5e8de9e61f60f8d2ee7b37e7de711
Sebastian <sa224ny@student.lnu.se>
2021-02-05 10:32:55 +0100

    Ants.java is done now

--- src/assignment_one/Ants.java
+++ src/assignment_one/Ants.java
@@ -1,5 +1,9 @@
 package assignment_one;
 
+/**
+ * Simulates how many steps it will take an ant to visit all squares on a
+ * chessboard.
+ */
 public class Ants {
   static int simCounter = 0;
 
@@ -11,9 +15,13 @@ public class Ants {
       resultsArray[i] = runGame();
     }
     System.out.println("Average amount of steps: " + getAverage(resultsArray));
-
   }
 
+  /**
+   * Runs a full simulation and counts the amount of steps the ant has to take.
+   * 
+   * @return the counter.
+   */
   static int runGame() {
     int counter = 0;
     String[][] chessBoard = new String[8][8];
@@ -30,15 +38,30 @@ public class Ants {
       chessBoard = takeStep(chessBoard);
       counter++;
     }
+    if (simCounter == 1) {
+      System.out.println("\nAnts\n=======");
+    }
     System.out.println("Number of steps in simulation " + simCounter + ": " + counter);
     return counter;
   }
 
+  /**
+   * Places the ant on the chessboard.
+   * 
+   * @return the chessboard with the ant on it.
+   */
   static String[][] startGame(String[][] chessboard, int x, int y) {
     chessboard[x][y] = "CurrentlyOn";
     return chessboard;
   }
 
+  /**
+   * Takes in the chessboard 2d array and returns true if the ant has yet to be on
+   * a square.
+   *
+   * @param chessboard as the 2d array.
+   * @return boolean value.
+   */
   static boolean checkBoard(String[][] chessboard) {
     for (int i = 0; i < 8; i++) {
       for (int j = 0; j < 8; j++) {
@@ -50,30 +73,33 @@ public class Ants {
     return false;
   }
 
+  /**
+   * Takes in the chessboard 2d array and simulates a step for the ant.
+   *
+   * @param chessboard as the 2d array.
+   * @return the 2d array that the ant has stepped on.
+   */
   static String[][] takeStep(String[][] chessboard) {
     int randomDirection = GameSRP.randomNumber(1, 4);
     int x = 0;
+    int otherX = 0;
     int y = 0;
+    int otherY = 0;
     for (int i = 0; i < 8; i++) {
       for (int j = 0; j < 8; j++) {
         if (chessboard[i][j] == "CurrentlyOn") {
-          x = i;
-          y = j;
-
-          chessboard[i][j] = "AntWasHere";
+          x = otherX = i;
+          y = otherY = j;
         }
       }
     }
-    // 1 is up.
+    // 1 = up, 2 = right, 3 = down and 4 = left.
     if (randomDirection == 1) {
       y++;
-      // 2 is right.
     } else if (randomDirection == 2) {
       x++;
-      // 3 is down.
     } else if (randomDirection == 3) {
       y--;
-      // 4 is left.
     } else {
       x--;
     }
@@ -81,17 +107,23 @@ public class Ants {
     if ((x < 0 || y < 0) || (x > 7 || y > 7)) {
       takeStep(chessboard);
     } else {
+      chessboard[otherX][otherY] = "AntWasHere";
       chessboard[x][y] = "CurrentlyOn";
     }
     return chessboard;
   }
 
+  /**
+   * Simple average calculator method.
+   * 
+   * @param resultsArray as the results to be "averaged".
+   * @return the average number as a double.
+   */
   static double getAverage(int[] resultsArray) {
     double total = 0;
     for (int i = 0; i < resultsArray.length; i++) {
       total = total + resultsArray[i];
     }
-
     return total / resultsArray.length;
   }
 
